fc7863a8d28693051411891795572b7d24ceee97
Gabriel Igliozzi <gaboiglio@gmail.com>
2022-10-11 22:15:40 -0400

    Added basic matrix functions declarations

--- src/gauss.h
+++ src/gauss.h
@@ -13,6 +13,13 @@ typedef struct {
     double** data;
 } matrix;
 
+typedef enum {
+    add,
+    substract,
+    multiply,
+    divide
+} operation;
+
 // All initializers of matrix
 void initMatrixFromInput(matrix* mat);
 void initMatrixFromFilepath(matrix* mat, const char* filepath);
@@ -20,11 +27,22 @@ void initMatrixRandomized(matrix* mat, unsigned int rows, unsigned int cols);
 void initMatrixFromArray(matrix* mat, unsigned int rows, unsigned int cols, double* arr);
 void initMatrixWithValue(matrix* mat, unsigned int rows, unsigned int cols, double value);
 
-// matrix destructor
+// Matrix destructor
 void freeMatrix(matrix* mat); 
 
-// outputting matrix
+// Outputting matrix
 void printMatrix(matrix* mat);
 void writeMatrix(matrix* mat, const char* filepath);
+
+// Scalar operations
+void scalarOperation(matrix* mat, double scalar, );
+
+
+// Basic matrix operations
+void rowSwap(matrix* mat, unsigned int row1, unsigned int row2);
+void colSwap(matrix* mat, unsigned int col1, unsigned int col2);
+void transpose(matrix* output, matrix* input);
+void addMatrix(matrix* result, matrix* mat1, matrix* mat2);
+void multiplyMatrix(matrix* result, matrix* mat1, matrix* mat2);
 
 #endif
